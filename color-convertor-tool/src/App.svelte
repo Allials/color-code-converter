42dc837e724d89d828bf672de0b9ee85135a9b87
Andrew Vasquez <103331877+Allials@users.noreply.github.com>
2025-02-19 12:02:21 -0600

    Setting up dependencies

---
+++ color-convertor-tool/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import './app.css'
+
+
+</script>
+
+<main>
+  <div class="min-h-screen dark:bg-gray-900 text-white ">
+    
+    <h1>Color Converter</h1>
+    
+ 
+ 
+ 
+  </div>
+
+
+
+
+</main>
+
